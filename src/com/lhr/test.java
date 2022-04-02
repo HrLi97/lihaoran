d14b0b5f19745fe6dad0cb4c1db93b7949924471
Xenogenesis1 <2809813570@qq.com>
2022-04-02 20:44:58 +0800

    new file

---
+++ src/com/lhr/test.java
@@ -0,0 +1,15 @@
+package com.lhr;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("fuck");
+    }
+
+    public void say(){
+        System.out.println("say");
+    }
+
+    public void talk(){
+        System.out.println("talk");
+    }
+}
